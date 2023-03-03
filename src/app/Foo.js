006da7b6024bf9344b399760a9fc0f876feee505
vinayraj <vinayraj.r@bhnetwork.com>
2023-03-03 09:32:08 +0530

    on Demand Import of functions

---
+++ src/app/Foo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Foo = () => {
+    return (
+        <h4>Inside Foo</h4>
+    )  
+}
+
---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from './Home';
+
+function App () {
+    return (
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    )
+}
+export default App;
